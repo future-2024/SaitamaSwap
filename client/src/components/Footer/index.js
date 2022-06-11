1d22139418648ac3ad1f401c789a8754b51a070b
FUTURE-2022 <97088619+Future-2022@users.noreply.github.com>
2022-06-12 06:21:54 +0900

    link fix

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,14 +8,13 @@ const Footer = () => {
     return (
         <>            
             <footer>
-                    <a href="https://www.SafeMoon.net/" target="_blank" rel="noreferrer">Website</a><a
-                            href="https://t.me/SafeMoonv2" target="_blank" rel="noreferrer">Telegram</a><a
-                            href="https://discord.com/invite/SafeMoon" target="_blank" rel="noreferrer">Discord</a><a
-                            href="https://github.com/SafeMoonprotocol"
-                            target="_blank" rel="noreferrer">Governance</a><a href="https://twitter.com/SafeMoon"
+                    <a href="https://www.saitamatoken.com/" target="_blank" rel="noreferrer">Website</a><a
+                            href="https://t.me/SaitamaWorldwide" target="_blank" rel="noreferrer">Telegram</a><a
+                            href="https://discord.com/invite/saitama" target="_blank" rel="noreferrer">Discord</a>
+                            <a href="https://twitter.com/WeAreSaitama"
                                                                             target="_blank" rel="noreferrer">Twitter</a><a
-                            href="https://www.bscscan.com/token/0x42981d0bfbAf196529376EE702F2a9Eb9092fcB5" target="_blank"
-                            rel="noreferrer">BSCscan</a><a href="https://www.SafeMoon.net/" target="_blank" rel="noreferrer">Contact
+                            href="https://etherscan.io/address/0xce3f08e664693ca792cace4af1364d5e220827b2" target="_blank"
+                            rel="noreferrer">BSCscan</a><a href="https://www.saitamatoken.com/" target="_blank" rel="noreferrer">Contact
                         us
                     </a>
                 </footer>
