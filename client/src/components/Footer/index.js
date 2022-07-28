f1ccf1d173d583544c5cc7faf2353cc04bf2f6b3
FUTURE-2022 <97088619+Future-2022@users.noreply.github.com>
2022-07-28 16:38:46 +0300

    up

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,16 +8,16 @@ const Footer = () => {
     return (
         <>            
             <footer>
-                    <a href="https://www.saitamatoken.com/" target="_blank" rel="noreferrer">Website</a><a
-                            href="https://t.me/SaitamaWorldwide" target="_blank" rel="noreferrer">Telegram</a><a
-                            href="https://discord.com/invite/saitama" target="_blank" rel="noreferrer">Discord</a>
-                            <a href="https://twitter.com/WeAreSaitama"
-                                                                            target="_blank" rel="noreferrer">Twitter</a><a
-                            href="https://etherscan.io/address/0xce3f08e664693ca792cace4af1364d5e220827b2" target="_blank"
-                            rel="noreferrer">BSCscan</a><a href="https://www.saitamatoken.com/" target="_blank" rel="noreferrer">Contact
-                        us
-                    </a>
-                </footer>
+                <a href="https://www.saitamatoken.com/" target="_blank" rel="noreferrer">Website</a><a
+                        href="https://t.me/SaitamaWorldwide" target="_blank" rel="noreferrer">Telegram</a><a
+                        href="https://discord.com/invite/saitama" target="_blank" rel="noreferrer">Discord</a>
+                        <a href="https://twitter.com/WeAreSaitama"
+                                                                        target="_blank" rel="noreferrer">Twitter</a><a
+                        href="https://etherscan.io/address/0xce3f08e664693ca792cace4af1364d5e220827b2" target="_blank"
+                        rel="noreferrer">BSCscan</a><a href="https://www.saitamatoken.com/" target="_blank" rel="noreferrer">Contact
+                    us
+                </a>
+            </footer>
         </>
     )
 }
